0d9ba34f16e3741d7ffcd6db7685b3ad3974012e
SimonWiddenhielm <55542256+SimonWiddenhielm@users.noreply.github.com>
2020-04-27 20:42:11 +0200

    Reset sql

---
+++ krusty/reset.sql
@@ -0,0 +1,88 @@
+
+
+
+
+
+
+SET FOREIGN_KEY_CHECKS = 0; 
+TRUNCATE TABLE customers;
+SET FOREIGN_KEY_CHECKS = 1;
+Insert into customers (Company,adress)
+VALUES('Bjudkakor AB','Ystad' ),('Finkakor AB','Helsingborg' ),('Gästkakor AB','Hässleholm' ),('Kaffebröd AB','Landskrona' )
+,('Kalaskakor AB','Trelleborg' ),('Partykakor AB','Kristianstad' ),('Skånekakor AB','Perstorp' ),('Småbröd AB','Malmö' );
+
+
+
+
+SET FOREIGN_KEY_CHECKS = 0; 
+TRUNCATE TABLE cookies;
+SET FOREIGN_KEY_CHECKS = 1;
+Insert into cookies (cookieName)
+VALUES('Almond delight' ),('Amneris' ),('Berliner' ),('Nut cookie' ),('Nut ring' ),('Tango');
+
+
+
+
+SET FOREIGN_KEY_CHECKS = 0; 
+TRUNCATE TABLE inventory;
+SET FOREIGN_KEY_CHECKS = 1;
+Insert into inventory (ingredient,quantity,unit)
+VALUES('Bread crumbs', 500000,'g'),('Butter', 500000,'g'),('Chocolate', 500000,'g'),('Chopped almonds', 500000,'g'),
+('Cinnamon', 500000,'g'),('Egg whites', 500000,'g'),('Eggs', 500000,'g')
+,('Fine-ground nuts', 500000,'g'),('Flour', 500000,'g'),('Ground, roasted nuts', 500000,'g'),('Icing sugar', 500000,'g'),('Marzipan', 500000,'g')
+,('Potato starch', 500000,'g'),('Roasted, chopped nuts', 500000,'g')
+,('Sodium bicarbonate', 500000,'g'),('Sugar', 500000,'g'),('Vanilla sugar', 500000,'g'),('Vanilla', 500000,'g'),('Wheat flour', 500000,'g');
+
+SET FOREIGN_KEY_CHECKS = 0; 
+
+
+
+
+
+TRUNCATE TABLE recipes;
+
+insert into recipes (cookieName,ingredient,quantity,unit)
+VALUES((SELECT * from cookies where cookieName = 'Almond delight'),(SELECT ingredient from inventory where ingredient = 'Butter'),400,'g' ),
+((SELECT * from cookies where cookieName = 'Almond delight'),(SELECT ingredient from inventory where ingredient = 'Chopped almonds'),279,'g' ),
+((SELECT * from cookies where cookieName = 'Almond delight'),(SELECT ingredient from inventory where ingredient = 'Cinnamon'),10,'g' ),
+((SELECT * from cookies where cookieName = 'Almond delight'),(SELECT ingredient from inventory where ingredient = 'Flour'),400,'g' ),
+((SELECT * from cookies where cookieName = 'Almond delight'),(SELECT ingredient from inventory where ingredient = 'Sugar'),400,'g' ),
+
+((SELECT * from cookies where cookieName = 'Amneris'),(SELECT ingredient from inventory where ingredient = 'Butter'),250,'g' ),
+((SELECT * from cookies where cookieName = 'Amneris'),(SELECT ingredient from inventory where ingredient = 'Eggs'),50,'g' ), 
+((SELECT * from cookies where cookieName = 'Amneris'),(SELECT ingredient from inventory where ingredient = 'Maripan'),750,'g' ), 
+((SELECT * from cookies where cookieName = 'Amneris'),(SELECT ingredient from inventory where ingredient = 'Potato starch'),,'g' ), 
+((SELECT * from cookies where cookieName = 'Amneris'),(SELECT ingredient from inventory where ingredient = 'Wheat flour'),100,'g' ), 
+
+((SELECT * from cookies where cookieName = 'Berliner'),(SELECT ingredient from inventory where ingredient = 'Butter'),250,'g' ),
+((SELECT * from cookies where cookieName = 'Berliner'),(SELECT ingredient from inventory where ingredient = 'Choclate'),50,'g' ), 
+((SELECT * from cookies where cookieName = 'Berliner'),(SELECT ingredient from inventory where ingredient = 'Eggs'),50,'g' ), 
+((SELECT * from cookies where cookieName = 'Berliner'),(SELECT ingredient from inventory where ingredient = 'Flour'),350,'g' ), 
+((SELECT * from cookies where cookieName = 'Berliner'),(SELECT ingredient from inventory where ingredient = 'Icing sugar'),100,'g' ), 
+((SELECT * from cookies where cookieName = 'Berliner'),(SELECT ingredient from inventory where ingredient = 'Vanilla sugar'),5,'g' ), 
+
+((SELECT * from cookies where cookieName = 'Nut cookie'),(SELECT ingredient from inventory where ingredient = 'Bread crumbs'),125,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut cookie'),(SELECT ingredient from inventory where ingredient = 'Chocolate'),50,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut cookie'),(SELECT ingredient from inventory where ingredient = 'Egg whites'),350,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut cookie'),(SELECT ingredient from inventory where ingredient = 'Fine-ground nuts'),750,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut cookie'),(SELECT ingredient from inventory where ingredient = 'Ground, roasted nuts'),625,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut cookie'),(SELECT ingredient from inventory where ingredient = 'Sugar'),375,'g' ), 
+
+((SELECT * from cookies where cookieName = 'Nut ring'),(SELECT ingredient from inventory where ingredient = 'Butter'),450,'g' ),
+((SELECT * from cookies where cookieName = 'Nut ring'),(SELECT ingredient from inventory where ingredient = 'Flour'),450,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut ring'),(SELECT ingredient from inventory where ingredient = 'Icing sugar'),190,'g' ), 
+((SELECT * from cookies where cookieName = 'Nut ring'),(SELECT ingredient from inventory where ingredient = 'Roasted, chopped nuts'),225,'g' ), 
+
+((SELECT * from cookies where cookieName = 'Tango'),(SELECT ingredient from inventory where ingredient = 'Butter'),200,'g' ), 
+((SELECT * from cookies where cookieName = 'Tango'),(SELECT ingredient from inventory where ingredient = 'Flour'),300,'g' ), 
+((SELECT * from cookies where cookieName = 'Tango'),(SELECT ingredient from inventory where ingredient = 'Sodium bicarbonate'),4,'g' ), 
+((SELECT * from cookies where cookieName = 'Tango'),(SELECT ingredient from inventory where ingredient = 'Sugar'),250,'g' ), 
+((SELECT * from cookies where cookieName = 'Tango'),(SELECT ingredient from inventory where ingredient = 'Vanilla'),2,'g' ), 
+
+SET FOREIGN_KEY_CHECKS = 0; 
+TRUNCATE TABLE cookieOrders;
+SET FOREIGN_KEY_CHECKS = 1;
+Truncate TABLE pallets;
+
+
+
