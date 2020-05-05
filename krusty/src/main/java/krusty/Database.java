8e1a3bbc63d107c8b3746ea7e6bd090d4bda8dee
marcussjokvist <55698855+marcussjokvist@users.noreply.github.com>
2020-05-05 18:17:56 +0200

    Added reset-method
    Reset method that reads from a .sql-file. Enabled multiple statements.

--- krusty/src/main/java/krusty/Database.java
+++ krusty/src/main/java/krusty/Database.java
@@ -2,7 +2,9 @@ package krusty;
 
 import spark.Request;
 import spark.Response;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,16 +13,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
-
 import com.mysql.jdbc.Driver;
-
 import static krusty.Jsonizer.toJson;
 
 public class Database {
 	/**
 	 * Modify it to fit your environment and then use this string when connecting to your database!
 	 */
-	private static final String jdbcString = "jdbc:mysql://puccini.cs.lth.se/hbg11";
+	private static final String jdbcString = "jdbc:mysql://puccini.cs.lth.se/hbg11?allowMultiQueries=true";
 
 
 	private static final String jdbcUsername = "hbg11";
@@ -130,7 +130,23 @@ public class Database {
 	}
 
 	public String reset(Request req, Response res) {
-		return "{}";
+		String response = "{\n\"status\": \"error\"\n}";
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("reset.sql"));
+			String str;
+			StringBuffer sb = new StringBuffer();
+			while ((str = reader.readLine()) != null) {
+				sb.append(str + " ");
+			}
+			reader.close();
+			PreparedStatement preStatement = connection.prepareStatement(sb.toString());
+			preStatement.executeQuery();
+			response = "{\n\"status\": \"ok\"\n}";
+		} catch (Exception e) {
+			System.err.println("Failed to Execute " + "reset" + ". The error is "+ e.getMessage());
+		}
+		return response;
 	}
 
 	public String createPallet( Request req, Response res) {
