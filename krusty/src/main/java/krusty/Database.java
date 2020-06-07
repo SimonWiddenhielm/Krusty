a5f7b48e888b47d9af5826fcbf50676eb33f20a3
SimonWiddenhielm <55542256+SimonWiddenhielm@users.noreply.github.com>
2020-06-07 11:29:54 +0200

    CAPSLOCK

--- krusty/src/main/java/krusty/Database.java
+++ krusty/src/main/java/krusty/Database.java
@@ -66,7 +66,7 @@ public class Database {
 
 
 	public String getRecipes(Request req, Response res) {
-		String sql = "SELECT cookieName as cookie,ingredient as raw_material,quantity as amount,unit from recipes";
+		String sql = "SELECT cookieName as cookie,ingredient as raw_material,quantity as amount,unit FROM RECIPES";
 		String name = "";
 		return getterSQL(sql,name);
 	}
